f43c1e93ad048897af54cb6998495a6aa447992a
tfk-ben <tbenkhelifa3@gmail.com>
2023-01-18 13:48:27 +0100

    add backend

--- main.tf
+++ main.tf
@@ -11,6 +11,16 @@ terraform {
     }
 
   }
+
+  # Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "terraform-storage"
+    storage_account_name  = "terrastat"
+    container_name        = "tfstatfiles"
+    key                   = "terraform.tfstate"
+  }  
+
+
 }
 
 provider "azurerm" {
